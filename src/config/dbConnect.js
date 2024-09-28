9c0c9dbbf0f905513b9bd84b693701c1815439f3
numberdaar <171ankit.2020@gmail.com>
2024-09-28 14:46:22 +0530

    add

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const dbConnect = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL, {
+    await mongoose.connect(process.env.MONGODB_URL, {
       autoIndex: true,
     });
     console.log("DB connected successfully!");
